ddfce47bf735d86bdb3cc5c657381d436da35686
DamianoTrifone <trifone.damiano@gmail.com>
2021-05-03 17:39:27 +0200

    Funzione pari o dispari

--- js/script2.js
+++ js/script2.js
@@ -10,5 +10,33 @@
 
 
 // Esercizio 2
-var pariDispari = parseInt(prompt("Pari o dispari? Inserisci un numero da 1 a 5"));
+var pariDispari = prompt("Pari o dispari? Inserisci la tua scelta");
 console.log(pariDispari);
+var numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+console.log(numeroUtente);
+
+function pariDispariPc(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var min = 1;
+var max = 5;
+var numeroPC = pariDispariPc(min, max);
+
+console.log(numeroPC);
+var somma = numeroUtente + numeroPC;
+console.log(somma);
+
+function pari(somma){
+    if(somma % 2 == 0){
+   return true;
+} else{
+    return false;
+}
+}
+
+if (pariDispari == pari && pari == true || paridispari == dispari && pari == false) {
+    document.getElementById("esito").innerHTML = "Hai vinto!"
+} else{
+    document.getElementById("esito").innerHTML = "Hai perso! Ritenta, sarai più fortunato"
+}
